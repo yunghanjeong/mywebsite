e05a6821051256af084949972096b91320189812
Yung Han Jeong <yhjung012@gmail.com>
2021-01-20 12:24:49 -0500

    update html formats

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ Created on Tue Jan 19 15:10:56 2021
 
 This code is to build a personal website based on Python Flask Library
 """
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home(): # home page
-    return "<h1> HELLO WORLD </h1> This is my website"
+    return render_template("index.html", content="Testing")
 
-@app.route("/<name>")
-def user(name): # home page
-    return f"hello {name}"
+# @app.route("/<name>")
+# def user(name): # home page
+#     return f"hello {name}"
 
-@app.route("/admin/")
-def admin(): # home page
-    return redirect(url_for("user", name="Admin!"))
+# @app.route("/admin/")
+# def admin(): # home page
+#     return redirect(url_for("user", name="Admin!"))
 
 if __name__ == "__main__":
     app.run()
